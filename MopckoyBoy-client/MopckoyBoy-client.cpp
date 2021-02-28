30873dd56db5a9aeb08f2d413f89edb73354e18c
noName <xkiksx2016@yandex.ru>
2021-02-28 17:10:02 +0400

    Добавьте файлы проекта.

---
+++ MopckoyBoy-client/MopckoyBoy-client.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Render.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) == 0)
+	{
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&RunWindow, NULL, NULL, NULL);
+		for (int i = 0; i < 100000; i++)
+			cout << i << endl;
+	}
+	else cout << SDL_GetError();
+
+	return 0;
+}
